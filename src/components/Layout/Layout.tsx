6aecd223ba11a3ccf03f0f2c1d905ee0343dc383
Sergey_Kondrashin <kondor85@mail.ru>
2024-10-03 00:13:01 +0300

    add icon

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,9 +1,18 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet } from 'react-router-dom'
 import ClimbersTabs from '../../features/climbers/ui/ClimbersTabs/ClimbersTabs'
-import Header from '../Header/Header';
+import Header from '../Header/Header'
+import img from '../../assets/climb.svg'
 
 const Layout = () => {
   return (<>
+    <img src={img} alt="" style={{
+      width: '200px',
+      position: 'fixed',
+      top: 0,
+      left: 0,
+      zIndex: 1000,
+      opacity: 0.2,
+    }}/>
     <Header />
       <aside className="fixed top-0 left-0 z-20 flex flex-col flex-shrink-0 w-96 h-full pt-16 duration-75 transition-width">
         <div className="pl-8 pt-8 pb-8"><ClimbersTabs /></div>
