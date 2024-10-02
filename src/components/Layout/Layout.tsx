2a664784b163b07776f977e024410a6ed94721e7
Sergey_Kondrashin <kondor85@mail.ru>
2024-10-02 16:44:15 +0300

    fix layout

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -5,10 +5,10 @@ import Header from '../Header/Header';
 const Layout = () => {
   return (<>
     <Header />
-      <aside className="fixed top-0 left-0 z-20 flex flex-col flex-shrink-0 w-64 h-full pt-16 duration-75 transition-width">
+      <aside className="fixed top-0 left-0 z-20 flex flex-col flex-shrink-0 w-96s h-full pt-16 duration-75 transition-width">
         <div className="p-8 mr-4"><ClimbersTabs /></div>
       </aside>
-      <main id="main-content" className="relative w-full h-full overflow-y-auto lg:ml-64 p-8">
+      <main id="main-content" className="relative w-full h-full overflow-y-auto ml-96 p-8">
         <Outlet />
       </main>
   </>);
