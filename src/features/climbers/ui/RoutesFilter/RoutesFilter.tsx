4beb9f47f00ac9a3bb94f941bd795a30217e78e4
Sergey_Kondrashin <kondor85@mail.ru>
2024-10-03 13:31:21 +0300

    disable for toggle

--- src/features/climbers/ui/RoutesFilter/RoutesFilter.tsx
+++ src/features/climbers/ui/RoutesFilter/RoutesFilter.tsx
@@ -19,10 +19,10 @@ const RoutesFilter = ({
     <div className="flex items-center mb-4">
       <div>
         <label className="inline-flex items-center cursor-pointer">
-          <span className="mr-2 text-sm font-medium text-gray-900">Боулдеринг</span>
+          <span className={`mr-2 text-sm font-medium ${settings.isLead ? 'text-gray-300' : 'text-gray-900'}`}>Боулдеринг</span>
           <input id={`isLead${prefix}`} type="checkbox" checked={settings.isLead} className="sr-only peer" onChange={(e) => onChange(e, 'isLead')} />
           <div className="relative w-11 h-6 bg-blue-600 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-blue-600"></div>
-          <span className="ml-2 text-sm font-medium text-gray-900">Трудность</span>
+          <span className={`ml-2 mr-2 text-sm font-medium ${settings.isLead ? 'text-gray-900' : 'text-gray-300'}`}>Трудность</span>
         </label>
       </div>
       <div className="ml-10">
