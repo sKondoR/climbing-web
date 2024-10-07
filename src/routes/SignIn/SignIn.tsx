115403cf70b7d0e8e503e169c765445334e8be2e
Sergey_Kondrashin <kondor85@mail.ru>
2024-10-08 01:49:55 +0300

    try svg animation

--- src/routes/SignIn/SignIn.tsx
+++ src/routes/SignIn/SignIn.tsx
@@ -1,6 +1,17 @@
+// @ts-ignore
+import Loading  from '../../components/Loading/Loading'
+
 const SignIn = () => {
-    return (<div className="flex justify-center">
-      <h3 className="m-6">Пожалуйста дождитесь окончания запроса профиля пользователя ВК</h3>
+    return (
+    <div>
+      <div className="flex flex-row justify-center">
+        <h3 className="m-6">Пожалуйста дождитесь окончания запроса профиля пользователя ВК</h3>
+      </div>
+      <div className="flex flex-row justify-center">
+        <div className="loading">
+          <Loading />
+        </div>
+      </div>
     </div>)
 }
   
