c3f174a2d2ad2dc45a23d13ab5a61d7327cb53d3
Sergey_Kondrashin <kondor85@mail.ru>
2024-10-07 11:35:44 +0300

    user icon

--- src/features/user/ui/UserIcon/UserIcon.tsx
+++ src/features/user/ui/UserIcon/UserIcon.tsx
@@ -1,15 +1,16 @@
 import { useUserStore } from '../../user.store'
 
-const SRC = 'https://s3.eu-central-1.amazonaws.com/allclimb-eu/storage/avatars/43779db00408615735c9e9265422d03df9259a8f.jpg'
 const UserIcon = () => {
   const { 
-    user,
+    vkUser,
   } = useUserStore()
+
+  if (!vkUser) return;
 
   return (
     <div>
-      {user && <h3>User: {user.name}</h3>}
-      <img src={SRC} alt="" />
+      {vkUser && <h3>vkUser: {vkUser.name}</h3>}
+      <img src= {vkUser.avatar_url as string} alt="" />
     </div>
   )
 }
