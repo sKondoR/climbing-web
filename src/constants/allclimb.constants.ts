ed0a197b68570e14f0127e46866a0f8f151f9ec2
Sergey_Kondrashin <kondor85@mail.ru>
2024-10-12 01:01:05 +0300

    update pro climbers

--- src/constants/allclimb.constants.ts
+++ src/constants/allclimb.constants.ts
@@ -1,10 +1,16 @@
 export const FRIENDS = [
     { allClimbId: 37751, name: 'Люба Бирилова' },
+    { allClimbId: 38689, name: 'Юля Никитина' },
     { allClimbId: 31377, name: 'Ксения Шерягина' },
     { allClimbId: 28360, name: 'skalolaz.pro' },
+    { allClimbId: 30093, name: 'Гавриил Шулев' },
 ];
 
 export const PRO = [
     { allClimbId: 26313, name: 'Анна Заикина' },
     { allClimbId: 17730, name: 'Дмитрий Факирянов' },
+    { allClimbId: 6508, name:  'Катя Киприянова' },
+    { allClimbId: 38689, name: 'Юля Никитина' },
+    { allClimbId: 38901, name: 'Катя Краморова' },
+    { allClimbId: 31001, name: 'Настя Зайкова' },
 ];
